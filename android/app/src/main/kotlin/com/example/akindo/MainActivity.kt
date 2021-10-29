d774568ce41476de29e45ff693a1cecb5e243082
Tani1015 <1015ryusuke1015@gmail.com>
2021-10-29 12:12:20 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/akindo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.akindo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+    );
+  }
+}
