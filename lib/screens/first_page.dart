a6fb884fcebd62fd250e628b30b9ddc0c8baedff
Tani1015 <1015ryusuke1015@gmail.com>
2021-11-02 06:21:51 +0900

    更新

---
+++ lib/screens/first_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class FirstPage extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: buildFirstPage(context)
+    );
+  }
+
+  buildFirstPage(BuildContext context){
+    return Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 25.0),
+      child: ListView(children: [
+        SizedBox(height: 250.0),
+        GestureDetector(
+          child: Container(
+            height: 50.0,
+            child: Material(
+              borderRadius: BorderRadius.circular(30.0),
+              shadowColor: Colors.greenAccent,
+              color: Colors.green,
+              elevation: 7.0,
+              child: Center(
+                child: Text("ユーザ登録"),
+              ),
+            ),
+          ),
+          onTap: (){
+            //タップ処理
+          },
+        ),
+        SizedBox(height: 70.0),
+        GestureDetector(
+          child: Container(
+            height: 50.0,
+            child: Material(
+              borderRadius: BorderRadius.circular(30.0),
+              shadowColor: Colors.greenAccent,
+              color: Colors.green,
+              elevation: 7.0,
+              child: Center(
+                child: Text("ログイン"),
+              ),
+            ),
+          ),
+          onTap: (){
+            //タップ処理
+          },
+        ),
+      ])
+    );
+  }
+}
