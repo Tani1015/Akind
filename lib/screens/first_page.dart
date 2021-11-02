1915e7bd6ff7fcbb157917969037ca2a554afd20
Tani1015 <1015ryusuke1015@gmail.com>
2021-11-02 16:40:07 +0900

    更新

--- lib/screens/first_page.dart
+++ lib/screens/first_page.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gradients/gradients.dart';
 
 class FirstPage extends StatelessWidget{
+  final colors = <Color>[Colors.lime,Colors.pink, Colors.purple];
   Widget build(BuildContext context){
     return Scaffold(
       body: buildFirstPage(context),
-      // backgroundColor: Color.fromRGBO(198,241,231,1),
-       backgroundColor:Colors.white,
+      backgroundColor: Color.fromRGBO(235,177,243,1),
+
     );
   }
 
   buildFirstPage(BuildContext context){
+
+
 
     return SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget> [
-            Container(
-              width: 330,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset('images/akind.jpg'),
+            Center(
+              child:Container(
+
+                width: 330,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('images/akind.jpg'),
+                ),
               ),
             ),
           SizedBox(height: 80.0),
@@ -33,8 +40,8 @@ class FirstPage extends StatelessWidget{
                   child: Material(
                     borderRadius: BorderRadius.circular(30.0),
                     shadowColor: Colors.greenAccent,
-                    color: Colors.cyanAccent,
-                    elevation: 7.0,
+                    color: Color.fromRGBO(136 , 255, 227, 1),
+                    elevation: 8.0,
                     child: Center(
                       child: Text("ユーザ登録"),
                     ),
@@ -53,9 +60,9 @@ class FirstPage extends StatelessWidget{
                 width: 300,
                 child: Material(
                   borderRadius: BorderRadius.circular(30.0),
-                  shadowColor: Colors.greenAccent,
-                  color: Colors.cyanAccent,
-                  elevation: 7.0,
+                  shadowColor: Colors.black,
+                  color: Color.fromRGBO(136 , 255, 227, 1),
+                  elevation: 8.0,
                   child: Center(
                     child: Text("ログイン"),
                   ),
