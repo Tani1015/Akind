c003c56b859c652f41cca96d312b4aaee8c855a4
Tani1015 <1015ryusuke1015@gmail.com>
2021-11-02 15:49:12 +0900

    ログインページ

--- lib/screens/first_page.dart
+++ lib/screens/first_page.dart
@@ -1,56 +1,121 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class FirstPage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
-      body: buildFirstPage(context)
+      body: buildFirstPage(context),
+      // backgroundColor: Color.fromRGBO(198,241,231,1),
+       backgroundColor:Colors.white,
     );
   }
 
   buildFirstPage(BuildContext context){
-    return Padding(
-        padding: const EdgeInsets.only(left: 20.0, right: 25.0),
-      child: ListView(children: [
-        SizedBox(height: 250.0),
-        GestureDetector(
-          child: Container(
-            height: 50.0,
-            child: Material(
-              borderRadius: BorderRadius.circular(30.0),
-              shadowColor: Colors.greenAccent,
-              color: Colors.green,
-              elevation: 7.0,
-              child: Center(
-                child: Text("ユーザ登録"),
+
+    return SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Container(
+              width: 330,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('images/akind.jpg'),
               ),
             ),
-          ),
-          onTap: (){
-            //タップ処理
-            Get.toNamed("/SignUp");
-          },
-        ),
-        SizedBox(height: 70.0),
-        GestureDetector(
-          child: Container(
-            height: 50.0,
-            child: Material(
-              borderRadius: BorderRadius.circular(30.0),
-              shadowColor: Colors.greenAccent,
-              color: Colors.green,
-              elevation: 7.0,
-              child: Center(
-                child: Text("ログイン"),
+          SizedBox(height: 80.0),
+          GestureDetector(
+                child: Container(
+                  height: 50.0,
+                  width: 300,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(30.0),
+                    shadowColor: Colors.greenAccent,
+                    color: Colors.cyanAccent,
+                    elevation: 7.0,
+                    child: Center(
+                      child: Text("ユーザ登録"),
+                    ),
+                  ),
+                ),
+                onTap: (){
+                  //タップ処理
+                  Get.toNamed("/SignUp");
+                },
+
               ),
-            ),
-          ),
-          onTap: (){
-            //タップ処理
-            Get.toNamed("/Login");
-          },
-        ),
-      ])
-    );
+            SizedBox(height: 40.0),
+            GestureDetector(
+              child: Container(
+                height: 50.0,
+                width: 300,
+                child: Material(
+                  borderRadius: BorderRadius.circular(30.0),
+                  shadowColor: Colors.greenAccent,
+                  color: Colors.cyanAccent,
+                  elevation: 7.0,
+                  child: Center(
+                    child: Text("ログイン"),
+                  ),
+                ),
+              ),
+              onTap: (){
+                //タップ処理
+                Get.toNamed("/Login");
+                },
+              ),
+          ],
+        ));
+
+
+
+
+
+
+    // return Padding(
+    //     padding: const EdgeInsets.only(left: 20.0, right: 25.0),
+    //   child: ListView(children: [
+    //     SizedBox(height: 550.0),
+    //     GestureDetector(
+    //       child: Container(
+    //         height: 50.0,
+    //         child: Material(
+    //           borderRadius: BorderRadius.circular(30.0),
+    //           shadowColor: Colors.greenAccent,
+    //           color: Colors.green,
+    //           elevation: 7.0,
+    //           child: Center(
+    //             child: Text("ユーザ登録"),
+    //           ),
+    //         ),
+    //       ),
+    //       onTap: (){
+    //         //タップ処理
+    //         Get.toNamed("/SignUp");
+    //       },
+    //     ),
+    //     SizedBox(height: 20.0),
+    //     GestureDetector(
+    //       child: Container(
+    //         height: 50.0,
+    //         child: Material(
+    //           borderRadius: BorderRadius.circular(30.0),
+    //           shadowColor: Colors.greenAccent,
+    //           color: Colors.green,
+    //           elevation: 7.0,
+    //           child: Center(
+    //             child: Text("ログイン"),
+    //           ),
+    //         ),
+    //       ),
+    //       onTap: (){
+    //         //タップ処理
+    //         Get.toNamed("/Login");
+    //       },
+    //     ),
+    //   ])
+    // );
   }
 }
